d0867ad4796f9a9599e022564eb7494e690ebf2d
gregwhitaker <gwhitake@gmail.com>
2018-04-28 17:05:17 -0700

    Doing work

--- stargazers-utils/users/contact_info.py
+++ stargazers-utils/users/contact_info.py
@@ -1,5 +1,4 @@
 import csv
-import datetime
 import json
 from os import listdir, makedirs
 from os.path import isfile, join, exists
@@ -7,6 +6,7 @@ from os.path import isfile, join, exists
 # Configuration
 DATA_DIR = '/Users/greg/workspace/stargazers-utils/tests/data'
 OUTPUT_DIR = '/Users/greg/workspace/stargazers-utils/tests'
+OUTPUT_FILENAME = 'contact_info.csv'
 
 
 class ContactInfo:
@@ -15,9 +15,9 @@ class ContactInfo:
     """
 
     def __init__(self):
-        """
-
-        """
+        with open(join(OUTPUT_DIR, OUTPUT_FILENAME), 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['login', 'name', 'email', 'company', 'bio', 'location'])
 
     def run(self):
         """
@@ -34,6 +34,8 @@ class ContactInfo:
         for f in data_files:
             print "Processing: {0}".format(f)
             json = self.__get_file_json(f)
+            contact_data = self.__get_contact_data(json)
+            self.__append_to_output_file(contact_data)
 
             processed_cnt += 1
 
@@ -55,16 +57,50 @@ class ContactInfo:
         """
         Strips the HTTP header off of the data file and returns a json string
         :param f: data file to process
-        :return: json string
+        :return: json
         """
         with open(f, 'r') as data_file:
             found = False
             for line in data_file:
                 if found:
-                    return line
+                    return json.loads(line)
                 else:
                     if len(line.strip()) == 0:
                         found = True
+
+    def __get_contact_data(self, json):
+        """
+
+        :param data_str:
+        :return:
+        """
+        contact_data = dict()
+        contact_data['login'] = json['login']
+        contact_data['name'] = json['name']
+        contact_data['email'] = json['email']
+        contact_data['company'] = json['company']
+        contact_data['bio'] = json['bio']
+        contact_data['location'] = json['location']
+
+        return contact_data
+
+    def __append_to_output_file(self, contact_data):
+        """
+
+        :param contact_data:
+        :return:
+        """
+        with open(join(OUTPUT_DIR, OUTPUT_FILENAME), 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+
+            row = [contact_data['login'],
+                   contact_data['name'],
+                   contact_data['email'],
+                   contact_data['company'],
+                   contact_data['bio'],
+                   contact_data['location']]
+
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
